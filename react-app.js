8dd7120faaf77c643f4d220a4e01055bf67f9b51
Sophia Di <backwardsxmarathon@gmail.com>
2017-05-31 14:50:10 -0400

    Add smart component which renders inputs to page linked up to api request function in main class.

--- react-app.js
+++ react-app.js
@@ -1,3 +1,75 @@
+class App extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			businesses: []
+		}
+		this.getData = this.getData.bind(this);
+	}
+
+	render() {
+		return(
+			<div>
+				<UserInput searchFunc={this.getData} />
+				<YelpResults results={this.state.businesses} />
+
+			</div>
+		)
+	}
+
+	getData(keywords, location) {
+		axios.get("https://yelp-search.herokuapp.com/search", {
+			params: {
+				location: location,
+				term: keywords
+			}
+		}).then((response) => {
+			this.setState({businesses: response.data.businesses})
+		});
+
+	}
+
+}
+
+class UserInput extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			keywords: "",
+			location: ""
+		}
+
+		this.handleSearch = this.handleSearch.bind(this);
+		this.updateKeywords = this.updateKeywords.bind(this);
+		this.updateLocation = this.updateLocation.bind(this);
+	}
+
+	render() {
+		return(
+			<div className="user-input__container">
+				<input onChange={this.updateLocation} type="text" placeholder="Enter location" />
+				<input onChange={this.updateKeywords} type="text" placeholder="Enter keywords" />
+				<button onClick={this.handleSearch}>Search Yelp</button>
+			</div>
+		)
+	}
+
+	updateKeywords(event) {
+		this.setState({keywords: event.target.value});
+	}
+
+	updateLocation(event) {
+		this.setState({location: event.target.value});
+	}
+
+	handleSearch() {
+		this.props.searchFunc(this.state.keywords, this.state.location);
+	}
+}
+
+
 function YelpResults(props) {
 
 	console.log(props);
@@ -13,20 +85,11 @@ function YelpResults(props) {
 
 	return(
 		<div>{businesses}</div>
+
 	)
 }
 
-axios.get('https://yelp-search.herokuapp.com/search', {
-	params: {
-		location: "Philadelphia",
-		term: "Pizza"
-	}
-}).then(function(response) {
-	let businesses = response.data.businesses;
-
-	ReactDOM.render(
-		<YelpResults results={businesses} />,
-		document.getElementById("react")
-	)
-
-});
+ReactDOM.render(
+	<App />,
+	document.getElementById("react")
+)
